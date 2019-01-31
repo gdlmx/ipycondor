a4c2e200d66f3e8730bd3e29158c0c455161b4fa
Lukas M. Koschmieder <lukas.koschmieder@rwth-aachen.de>
2019-01-31 10:26:00 +0100

    Add grid options

--- ipycondor/Qgrid.py
+++ ipycondor/Qgrid.py
@@ -7,5 +7,7 @@ def to_qgrid(data, columns, index=None):
     df = pd.DataFrame(data, columns=columns)
     if index: df = df.set_index(index)
     widget = qgrid.show_grid(df, show_toolbar=False,
-        grid_options={'editable':False})
+        grid_options={'editable':False,
+                      'minVisibleRows':0,
+                      'defaultSortAsc':True})
     return widget
