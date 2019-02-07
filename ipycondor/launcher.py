9b0967bb39303462d67ab56f3ea8930771eeb52a
Mingxuan Lin <gdlmx@users.noreply.github.com>
2019-02-07 14:14:24 +0100

    Pipe ssh_to_job output to the log

--- ipycondor/launcher.py
+++ ipycondor/launcher.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Mingxuan Lin
 
-import time, json, os, subprocess, socket
+import time, json, os, subprocess, socket, io
 from ipyparallel.apps.launcher import HTCondorLauncher, BatchClusterAppMixin, ioloop
 from traitlets import (Any, Integer, List, Unicode, default)
 # CFloat,Dict, Instance, HasTraits, CRegExp, TraitError, validate, observe
@@ -16,9 +16,9 @@ transfer_executable  = true
 transfer_input_files ={to_send}
 should_transfer_files= yes
 
-stream_output = true
+#stream_output = true
 stream_error  = true
-output = ipyengine.$(ClusterId).$(ProcId).out.txt
+#output = ipyengine.$(ClusterId).$(ProcId).out.txt
 error  = ipyengine.$(ClusterId).$(ProcId).err.txt
 log    = ipyengine.$(ClusterId).$(ProcId).log
 
@@ -94,7 +94,10 @@ queue
             if jstat == 2: #running
                 self.poller.callback_time = 20*1000
                 if not self.job_is_local and self.ssh_stat() == 'none':
-                    self.create_tunnel()
+                    try:
+                        self.create_tunnel()
+                    except: #pylint: disable=W0702
+                        self.stop()
             elif jstat in (3, 4, 6): # stopped. No further action on the job is needed
                 self.notify_stop(jstat)
         elif jstat != 2 and time.time() > self.job_submit_time + self.job_timeout:
@@ -129,21 +132,25 @@ queue
         with open(self.ipcontroller_info, 'r') as f:
             stat_engine=json.load( f )
 
-        args=['condor_ssh_to_job', str(self.job_id), '-N']
+        args=['condor_ssh_to_job', str(self.job_id), '-N', '-v', '-o', "ExitOnForwardFailure yes"]
         for k in ("registration", "control", "mux", "hb_ping", "hb_pong", "task", "iopub" ):
             args += ['-R', 'localhost:{0}:localhost:{0}'.format(stat_engine[k])]
 
         p=subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         self.ssh_to_job_proc = p
         try:
-            out,err=p.communicate(timeout=2)
-            self.ssh_to_job_proc = None
-            raise RuntimeError(out,err)
+            ret_code = p.wait(timeout=2)
         except subprocess.TimeoutExpired:
             pass
-
-        self.log.info('`condor_ssh_to_job` started (PID=%d)', p.pid)
-        self.on_stop(self.stop_ssh_tunnel) # register as a callback (triggered by `notify_stop`)
+        else:
+            out, err = tuple(x.decode(errors='ignore') for x in p.communicate(timeout=1))
+            self.ssh_to_job_proc = None
+            self.log.error('condor_ssh_to_job failed too early [%d]\n\t%s\n\t%s', ret_code, out, err)
+            raise RuntimeError(err)
+        self.on_stop(self.stop_ssh_tunnel)
+        #self.ssh_stderr = SubprocPipeBuf(self.loop, p, 'stderr').buf
+        SubprocPipeBuf(self.loop, p, 'stderr', lambda l: self.log.debug('[SSH] - %s', l.rstrip()))
+        self.log.info('condor_ssh_to_job started successfully (PID=%d)', p.pid)
 
     def ssh_stat(self):
         p = self.ssh_to_job_proc
@@ -151,15 +158,18 @@ queue
             return 'running' if p.poll() is None else 'exited'
         return 'none' if not p else 'creating'
 
-    def stop_ssh_tunnel(self, cb_data=None):
+    def stop_ssh_tunnel(self, cb_data=None): #pylint: disable=W0613
         p = self.ssh_to_job_proc
         if self.ssh_stat() == 'running':
             p.terminate()
-            out, err = tuple(x.decode(errors='ignore') for x in p.communicate(timeout=1))
-            self.log.debug('`condor_ssh_to_job` %s exited with %s: %s\n\t%s', p.pid, p.poll(), out, err)
+            try:
+                out, err = tuple(x.decode(errors='ignore') for x in p.communicate(timeout=1))
+                self.log.debug('`condor_ssh_to_job` %s exited with %s: %s\n\t%s', p.pid, p.poll(), out, err)
+            except subprocess.TimeoutExpired:
+                pass
 
 def wait_for_new_file(filename, timeout=20):
-    for i in range(timeout):
+    for i in range(timeout): #pylint: disable=W0612
         try:
             fileage = time.time() - os.stat(filename).st_mtime
             if ( fileage<timeout and fileage>0 ):
@@ -167,3 +177,24 @@ def wait_for_new_file(filename, timeout=20):
         except FileNotFoundError:
             time.sleep(1)
     return False
+
+class SubprocPipeBuf:
+    def __init__(self, loop, proc, pipename='stdout', line_callback=None):
+        self.pipe = getattr(proc, pipename)
+        self.buf  = None
+        if line_callback:
+            self.line_callback = line_callback
+        else:
+            self.buf  = io.StringIO()
+            self.line_callback = self.buf.write
+        self.loop = loop
+        self.proc = proc
+        loop.add_handler(self.pipe, self._read_handler, loop.READ)
+
+    def _read_handler(self, fd, evt): #pylint: disable=W0613
+        l = self.pipe.readline().decode('utf8', 'replace')
+        if l:
+            self.line_callback(l)
+        else:
+            if self.proc.poll() is not None:
+                self.loop.remove_handler(self.pipe)
